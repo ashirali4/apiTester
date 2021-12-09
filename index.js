51f48c7a2e3992144f446e07318376aa13dd4868
ashir.muhammad <ALIali123!>
2021-12-09 19:48:54 +0500

    sf

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, true   );
 var channel = "ciao"
+const PORT = process.env.PORT || 3000                                                   
 
 
 app.get('/', (req, res) => {
@@ -20,6 +21,6 @@ io.on('connection', (socket) => {
         });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('listening on *:3000');
 });
